ed328357423571260e160abab65491d6e6bc860f
Pratuesh Anand <pratueshanand2002@gmail.com>
2023-02-24 21:19:34 +0530

    Faq Questions and updated team page

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -4,33 +4,192 @@ const FAQ = [
     {
         ques:"Will the layout of flat be changed?",
         ans:`Layout can not be changed but if members want to upgrade their units / or shift to another tower in Phase -1 then â€“ may so be done at the time of BBA change as follows
-            -   Receiving request from Members, 
-            -	Availability of flat will be ascertained 
-            -	Then first-come, first-served basis
+        <br>    -   Receiving request from Members, 
+        <br>    -	Availability of flat will be ascertained 
+        <br>    -	Then first-come, first-served basis
         `
     },
     {
-        ques:"Will the layout of flat be changed?",
-        ans:`Layout can not be changed but if members want to upgrade their units / or shift to another tower in Phase -1 then â€“ may so be done at the time of BBA change as follows
-            -   Receiving request from Members, 
-            -	Availability of flat will be ascertained 
-            -	Then first-come, first-served basis
+        ques:"Will the tower be changed?",
+        ans:`Tower can be changed at the time of BBA change after 
+        <br>    -   receiving request from Members
+        <br>    -	Availability of flat will be ascertained 
+        <br>    -	Then first-come, first-served basis
         `
     },{
-        ques:"Will the layout of flat be changed?",
-        ans:`Layout can not be changed but if members want to upgrade their units / or shift to another tower in Phase -1 then â€“ may so be done at the time of BBA change as follows
-            -   Receiving request from Members, 
-            -	Availability of flat will be ascertained 
-            -	Then first-come, first-served basis
+        ques:"Will the cost of flat change",
+        ans:`To begin with the cost of flat would remain same and will not change unless hard pressed in financials. However, keep provision of Rs. 150 to 250 per Sq. Feet on BSP to offset any inflation in construction cost, services, etc. Exact amount would be confirmed later based on overall actual financial health.
         `
     },{
-        ques:"Will the layout of flat be changed?",
-        ans:`Layout can not be changed but if members want to upgrade their units / or shift to another tower in Phase -1 then â€“ may so be done at the time of BBA change as follows
-            -   Receiving request from Members, 
-            -	Availability of flat will be ascertained 
-            -	Then first-come, first-served basis
+        ques:"What are future prospects of that are coming in the area around the project?",
+        ans:`The scenario of the Noida and Greater Noida has been drastically changed in last 18 months or so. There are huge developments being done / planned which includes 
+        <br>-	New JEWAR International Airport (proposed to go live in end of 2022), 
+        <br>-	Helipads, 
+        <br>-	New Film City, 
+        <br>-	New Industrial areas for new industries, 
+        <br>-	Various government projects, 
+        <br>-	Bullet Metro (Delhi-GN-Banaras) 
+        <br>-	Railway Corridor, Cargo Corridor, etc are launched. 
+        <br>-	Asian largest Logistic Hub is been proposed.
+        <br>-	State / Central governments are investing, along with industries coming in. 
+        <br>    So, International and Natioal people & Investers are looking forward to be
+            there, fortunately for SLB welfare Members, itâ€™s an opportunity and blessing
+            which all must grab.
         `
-    },
+    },{
+        ques:"When the project will be completed?",
+        ans:`Overall project is expected to be completed in 4 years. The project will be completed phase wise. First phase is planned to delivered in 2 years and completion also depends on collection of funds from the buyers.
+        `
+    },{
+        ques:"Where will this initial amount be used which is being collected now?",
+        ans:`The collected money would be spend on mobilization on the Project like as - salaries, professional fees, payment against work, payments for materials etc: 
+        <br>-	Hiring of staffs and professionals like Project Managers, CRM, Marketing, Accountant, Engineers, Technical Team, Consultants, Lawyers, Architect, etc.
+        <br>-	Electricity Connection onsite
+        <br>-	Water connection onsite
+        <br>-	Security Onsite
+        <br>-	Advertisement & Notices 
+        <br>-	Site cleaning, testing and assessment of old setup
+        <br>-	Sample flat 
+        <br>-	Site Office and Office bearing
+        <br>-	Board, Flex, Banners
+        <br>-	Printing & Stationary 
+        <br>-	Project Site fencing Etc.
+        <br>-	Construction on site
+        `
+    },{
+        ques:"Will the initial mobilization amount (2.5L) be deducted from cost of flat and will I get receipt?",
+        ans:`The amount contributed by buyers will be adjusted in Flat Money
+        <br>-	Deducted from the BSP  / OR
+        <br>-	remaining amount due.
+        <br>    Yes Receipt will be issued for amount received. 
+        `
+    },{
+        ques:"When will banks approve this project?",
+        ans:`SLBWA is in the process to get this project bankable and discussion are going on with PSU and Private banks. However, financial institutions will expedite the process of funding after project is live and running, which can be achieved by funds flow from buyers.
+        `
+    },{
+        ques:"When will be the BBA changed and what are the financial obligations?",
+        ans:`BBA would be almost similar, which has been worked out and shared with UP-RERA committee for their inputs and approval except few points aligned as per court order. The approved BBA will be changed after initial amount paid for project mobilization and minor financial* change (if hard pressed).
+        <br>*INR 150 to 250 on BSP against price escalation in construction cost, services, etc in last 10 years depending on overall financial health of project.
+        `
+    },{
+        ques:"How we can proceed if we want to change to first phase?",
+        ans:`At the time of BBA change, Members can make change in Location/ Tower after 
+        <br>-	receiving request from Members, 
+        <br>-	availability of flat will be ascertained 
+        <br>-	then first-come, first-served basis
+        `
+    },{
+        ques:"Is this project viable and approved by UP-RERA?",
+        ans:`The DPR for this project was submitted to the Authorities to ascertain viability which has already been verified, audited by UP-RERA and Government. Further it been verified by Technical consultant appointed by SLBWA. Upon so many checks and audits on viability of this project, this is granted to SLBWA for it further development and construction.
+        `
+    },{
+        ques:"Is Escrow account opened for this project and where will be the money be used from this account?",
+        ans:`The Escrow account is available and details has been shared through emails to all respective members of SLBWA. Money of this account will be only used for this Sampada Livia Project upon proper approval process from respective subject matter experts, authorities, association after verification of work.
+        `
+    },{
+        ques:"From where can we collect membership ID cards?",
+        ans:`One can collect ID cards from the construction site itself or contact Mr. Santosh Bhardwaj.
+        `
+    },{
+        ques:"When can we visit the construction site?",
+        ans:`Anytime, please carry valid Membership ID card.
+        `
+    },{
+        ques:"Where is / will be the office of the project be situated ?",
+        ans:`Office would be at the project site. If needed in future a office can be estabised in NOIDA, but financial have to considered and fund flow has to be there for the expenses.
+        `
+    },{
+        ques:"Will there be any price escalation?",
+        ans:`To begin with the cost of flat would remain same and will not change unless hard pressed in financials. However, keep provision of Rs. 150 to 250 per Sq. Feet on BSP to offset any inflation in construction cost, services, etc if hardpressed. Exact amount would be confirmed later based on overall actual financial health.
+        `
+    },{
+        ques:"Where we can check the progress of the project?",
+        ans:`Progress of the project can be checked at UP-RERA and SLBWA website. Members will be updated on WhatsApp group also.
+        `
+    },{
+        ques:"What GST will be applicable for the our payments?",
+        ans:`GST will be applicable as per prevailing Govt policy.
+        `
+    },{
+        ques:"When will be the BBA changed along with further payment plan",
+        ans:`BBA will be changed after the initial amount of 2.5 lakh have been given for mobilization of the Project. Further payment shedule will be shared along the BBA to the Allottee(s).
+        `
+    },{
+        ques:"Is the awarded contractor has experience in high rise building and how it was verified?",
+        ans:`Yes contractor is qualified and experienced in construction of multi-storyed / high rise residential buildings. Completed projects provided by contractor were verified by the consultant and Team.
+        <br>
+        <br>List of projects done by them, specific to high rise for verification:
+        <br>
+        <br>1.	Gardenia Glory, Sec 46, Noida
+        <br>2.	Geetanjali, Vasundhra
+        <br>3.	Raksha Addela, Gaur City, Greater Noida West
+        <br>4.	Golf City, Sector 75, Noida
+        <br>    Basis on the list shared by bidder AKSHITA, there was physical verification been done for three projects mentioned below by the technical team as per tendering process on dated 30th July 2020 to ascertain the work . 
+        <br>
+        <br>A.	GARDENIA GLORY 
+        <br>    SECTOR â€“ 46, NOIDA 
+        <br>    NUMBER OF FLATS ~ 1950 
+        <br>
+        <br>B.	GEETANJALI 
+        <br>    SECTOR â€“ 18, VASUNDRA
+        <br>    NUMBER OF FLATS ~215
+        <br>
+        <br>C.	GOLF CITY 
+        <br>    SECTOR 75, NOIDA
+        <br>    NUMBER OF FLATS ~ 2500 
+        <br>
+        <br>Upon physical visit on site and further being verified by these projects Promoters (owners), the work done by bidder was satisfactory.
+        `
+    },{
+        ques:"How will be the governance of Project activites?",
+        ans:`Project activity can be seen:
+        <br>
+        <br>1.	All details will be seen on the UP-RERA website under the rehabilitation project / SAMPADA LIVIA.
+        <br>2.	All the details of DPR which has been verified and audited by the technical team appointed by Association and by UP-RERA Monitoring committee has been uploaded to UP-RERA website.
+        <br>3.	The details of Auditor / Technical teams / Architect / Contractor / Project Manager etc. has been uploaded to UP-RERA website.
+        <br>4.	All the detailed documents about whole project along with different authorities, maps, layout etc. are uploaded to UP-RERA website.
+        <br>5.	BBA (vetted and approved by UP-RERA) submitted by Association uploaded to UP-RERA website.
+        <br>6.	Building plan along with progress which has to be done with timeline boundation has been uploaded to UP-RERA website.
+        <br>7.	The progress of all the departmentsâ€™ work and funds will be verified and audited by:
+        <br>-	Technical team appointed by the Association
+        <br>-	Project Engineer and Quality Controller appointed by Association
+        <br>-	Architect appointed by the Association
+        <br>-	CA and Accountant appointed by the Association Upon above checks, it will be submitted to UP-RERA committee, which will be 
+        further examined & audited by:
+        <br>-	Technical expert appointed by UP  - RERA committee
+        <br>-	Auditor appointed by UP- RERA committee
+        <br>-	Core Monitoring UP- RERA committee members
+        <br>
+        <br>IMPORTANT NOTE: FOR TIMELY COMPLETION OF THE PROJECT, ALL THE BUYERS TIMELY PAYMENT IS CRITICAL AGAINST DEMAND.
+        `
+    },{
+        ques:"How can we become member of SLBWA?",
+        ans:`Please send duly filled and signed Declaration forms (3 Copies), 6 passport size photos and following documents for documentation 
+        <br>-	BBA
+        <br>-	Receipt 
+        <br>-	Bank statements 
+        <br>-	Loan bank Statement 
+        <br>-	Loan disbursal letter if any
+        <br>-	Allotment letter 
+        <br>-	Allottee Aadhar car
+        <br>
+        <br>Make 2 photocopies file and send at below mentioned address:
+        <br>
+        <br>SLB WELFARE ASSOCIATION 
+        <br>SAMPADA LIVIA
+        <br>GH-06B, SECTOR â€“ CHI-V, 
+        <br>2ND MILESTONE, EXPRESSWAY
+        <br>GREATER NOIDA 
+        <br>DISTT: GAUTAM BUDDHA NAGAR 
+        <br>UTTAR PRADESH
+        <br>
+        <br>Once documents are verified, you are required to pay membership fee and annual 
+        fee.
+        <br>
+        <br>Consent / Declaration FORM COPY ATTACHED BELOW        
+        `
+    }
 ]
 
 const faqGenerator = (faq, idx) => {
