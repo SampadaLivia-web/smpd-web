993571fa9072b2c9636442a4397824dbf64af521
Gupta-Nikhil85 <nikhilgupta8800@gmail.com>
2023-02-24 15:18:33 +0530

    initial about page added.

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -3,8 +3,8 @@ headerRef.innerHTML=`<div class="container d-flex align-items-center justify-con
 <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="SampadaLivia" class="img-fluid"></a>
 <nav id="navbar" class="navbar order-last order-lg-0">
   <ul>
-    <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-    <li><a class="nav-link scrollto" href="#about">About</a></li>
+    <li><a class="nav-link scrollto" href="./index.html">Home</a></li>
+    <li><a class="nav-link scrollto" href="./about.html">About</a></li>
     <li><a class="nav-link scrollto" href="./support.html">FAQs</a></li>
     <li><a class="nav-link scrollto" href="#team">Team</a></li>
     <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
