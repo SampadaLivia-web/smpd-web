d81200ba8238e7d53578e99162baf68b22f1f11b
Pratuesh Anand <pratueshanand2002@gmail.com>
2023-02-28 00:27:11 +0530

    footer updated

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -16,11 +16,11 @@ footerRef.innerHTML=`<div class="footer-top">
     <div class="col-lg-2 col-md-6 footer-links">
       <h4>Useful Links</h4>
       <ul>
-        <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-        <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-        <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-        <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-        <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+        <li><i class="bx bx-chevron-right"></i> <a href="./index.html">Home</a></li>
+        <li><i class="bx bx-chevron-right"></i> <a href="./about.html">About us</a></li>
+        <li><i class="bx bx-chevron-right"></i> <a href="./support.html">FAQs</a></li>
+        <li><i class="bx bx-chevron-right"></i> <a href="./team.html">Team</a></li>
+        <li><i class="bx bx-chevron-right"></i> <a href="./contact.html">Contact</a></li>
       </ul>
     </div>
 
