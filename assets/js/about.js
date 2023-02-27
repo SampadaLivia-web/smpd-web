abf3061e000e6de3b790b13f0484c839dd030602
Gupta-Nikhil85 <nikhilgupta8800@gmail.com>
2023-02-27 23:28:30 +0530

    about us updated

--- assets/js/about.js
+++ assets/js/about.js
@@ -44,21 +44,21 @@ const getProduct = (product, idx) => {
     return `
         <div class="my-4 col-md-6 col-lg-4">
             <div class="shadow rounded">
-            <a href=${product.toggle ? product.image1:product.image2} target="_blank">
-                <div class="h-400" style="background-image: url(${product.toggle ? product.image1:product.image2});"></div>
-            </a>
-            <blockquote class="blockquote p-4">
-                <h3 class="mb-5">${product.heading}</h3>
-                <footer class="blockquote-footer">${product.desc}</footer>
-                <div class="d-flex">
-                    <div class="btn btn-outline-dark d-flex justify-content-center align-items-center" id="${2*idx}">
-                        <i class="bi bi-arrow-left"></i>
+                <a href=${product.image1} target="_blank">
+                    <div class="h-400" style="background-image: url(${product.image1});"></div>
+                </a>
+                <blockquote class="blockquote p-4">
+                    <h3 class="mb-5">${product.heading}</h3>
+                    <footer class="blockquote-footer">${product.desc}</footer>
+                    <div class="d-flex">
+                        <div class="btn btn-outline-dark d-flex justify-content-center align-items-center">
+                            <i class="bi bi-arrow-left"></i>
+                        </div>
+                        <div class="btn btn-outline-dark d-flex justify-content-center align-items-center mx-2">
+                            <i class="bi bi-arrow-right"></i>
+                        </div>
                     </div>
-                    <div class="btn btn-outline-dark d-flex justify-content-center align-items-center mx-2" id="${2*idx+1}">
-                        <i class="bi bi-arrow-right"></i>
-                    </div>
-                </div>
-            </blockquote>
+                </blockquote>
             </div>
         </div>
     `
@@ -73,12 +73,3 @@ for(const product of products) {
 }
 
 productRef.innerHTML=productInnerHTML;
-
-var IconDivRef = document.getElementsByClassName('btn-outline-dark');
-
-
-for(const icon of IconDivRef){
-    icon.addEventListener('click', function(event){
-        console.log(event);
-    })
-}
