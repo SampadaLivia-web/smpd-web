251ca983a0d079ae95c6ade5cb2887bf2f8d7a95
Gupta-Nikhil85 <nikhilgupta8800@gmail.com>
2023-02-27 23:55:52 +0530

    about and contact content updated.

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,6 +1,6 @@
 var headerRef = document.getElementById("header");
 headerRef.innerHTML=`<div class="container d-flex align-items-center justify-content-lg-between">
-<a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="SampadaLivia" class="img-fluid"></a>
+<a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="Sampada Livia" class="img-fluid"></a>
 <nav id="navbar" class="navbar order-last order-lg-0">
   <ul>
     <li><a class="nav-link scrollto" href="./index.html">Home</a></li>
